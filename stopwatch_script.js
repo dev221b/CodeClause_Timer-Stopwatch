7ccff4db54a2a4543c8753bf4213942bafcb8e8d
Devika <devikab2021@gmail.com>
2023-05-28 15:12:01 +0530

    implemented basic functionality

---
+++ stopwatch_script.js
@@ -0,0 +1,69 @@
+window.onload = function(){
+      var minutes = 00;
+      var seconds = 00;
+      var centisecs = 00;
+      var dispMins = document.getElementById("min");
+      var dispSecs = document.getElementById("secs");
+      var dispCentiSecs = document.getElementById("centi");
+      var buttonStart = document.getElementById("btnStart");
+      var buttonStop = document.getElementById("btnStop");
+      var buttonReset = document.getElementById("btnReset");
+      var interval;     
+
+      buttonStart.addEventListener('click', () =>
+      {
+        interval = setInterval(timerun, 10);
+        
+      })
+
+      function timerun()
+      {
+        centisecs++;
+
+        if(centisecs<10)
+        centisecs = '0' + centisecs;
+        dispCentiSecs.innerHTML = centisecs;
+
+        if(centisecs > 99)
+        {
+          seconds++;
+          if(seconds<10)
+          seconds = "0" + seconds;
+          dispSecs.innerHTML = seconds;
+          centisecs = 0;
+          dispCentiSecs.innerHTML = '0' + centisecs;
+        }
+
+        if(seconds>59)
+        {
+          minutes++;
+          if(minutes<10)
+          minutes = '0' + minutes;
+          dispMins.innerHTML = minutes;
+          seconds = 0;
+          dispSecs.innerHTML = '0' + seconds;
+
+        }
+
+        buttonStop.addEventListener('click', () =>
+        {
+          clearInterval(interval);
+          
+        });
+
+        buttonReset.addEventListener('click', () =>
+        {
+          clearInterval(interval);
+          centisecs = "00";
+          seconds = "00";
+          minutes = "00";
+          dispCentiSecs.innerHTML = centisecs;
+          dispSecs.innerHTML = seconds;
+          dispMins.innerHTML = minutes;
+          
+        });
+        
+      }
+    }
+
+    
---
+++ timer_script.js
@@ -0,0 +1,71 @@
+var countdown;
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+
+function startCountdown() {
+    var inputHours = parseInt(document.getElementById("input-hours").value) || 0;
+    var inputMinutes = parseInt(document.getElementById("input-minutes").value) || 0;
+    var inputSeconds = parseInt(document.getElementById("input-seconds").value) || 0;
+  
+    // Calculate total seconds
+    var totalSeconds = (inputHours * 3600) + (inputMinutes * 60) + inputSeconds;
+  
+    // Calculate hours, minutes, and seconds from total seconds
+    hours = Math.floor(totalSeconds / 3600);
+    minutes = Math.floor((totalSeconds % 3600) / 60);
+    seconds = totalSeconds % 60;
+  
+    countdown = setInterval(updateCountdown, 1000);
+}
+
+function updateCountdown() {
+    if (seconds > 0) {
+        seconds--;
+      } else {
+        if (minutes > 0) {
+          minutes--;
+          seconds = 59;
+        } else {
+          if (hours > 0) {
+            hours--;
+            minutes = 59;
+            seconds = 59;
+          } else {
+            // Countdown completed
+            stopCountdown();
+            resetCountdown();
+  alert("Countdown completed!");
+            return;
+          }
+        }
+    }
+
+    document.getElementById("hours").textContent = padZero(hours);
+    document.getElementById("minutes").textContent = padZero(minutes);
+    document.getElementById("seconds").textContent = padZero(seconds);
+}
+
+function stopCountdown() {
+  clearInterval(countdown);
+  console.log("Hello")
+  return;
+}
+
+function resetCountdown() {
+    clearInterval(countdown);
+    document.getElementById("input-hours").value = "";
+    document.getElementById("input-minutes").value = "";
+    document.getElementById("input-seconds").value = "";
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    document.getElementById("hours").textContent = "00";
+    document.getElementById("minutes").textContent = "00";
+    document.getElementById("seconds").textContent = "00";
+    return;
+  }
+
+function padZero(value) {
+  return value.toString().padStart(2, "0");
+}
